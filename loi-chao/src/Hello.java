68cc8fa12a38a5aaf21cbb22afb77816f2d2a27c
TAi <daudangvua@gmail.com>
2025-04-07 09:22:56 +0700

    add Hello

---
+++ loi-chao/src/Hello.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Hello {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập tên của bạn: ");
+        String name = scanner.nextLine();
+        System.out.println("Xin chào: " + name);
+    }
+}
