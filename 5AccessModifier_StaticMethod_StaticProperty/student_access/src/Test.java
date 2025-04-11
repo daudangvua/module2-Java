6eb0c15d0badd433600e67e86d8c10246558832f
TAi <daudangvua@gmail.com>
2025-04-11 11:43:01 +0700

    add Student

---
+++ 5AccessModifier_StaticMethod_StaticProperty/student_access/src/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.setName("li");
+        s.setClasses("C01");
+        System.out.printf("tên:"+ s.getName()+" - lớp:"+s.getClasses());
+    }
+}
