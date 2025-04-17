c762812ab700916c19e7db42a59467e68ce9277f
TAi <daudangvua@gmail.com>
2025-04-17 10:57:37 +0700

    add minitest bai8

--- _9kiem_thu/src/TestDay.java
+++ _9kiem_thu/src/TestDay.java
@@ -3,8 +3,6 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class TestDay {
     @Test
     @DisplayName("Testing")
