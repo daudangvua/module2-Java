f6d7c834583067da6eb5304b27ac29b03133d043
TAi <daudangvua@gmail.com>
2025-04-21 16:29:02 +0700

    add bt

--- 11 DSA Stack Queue/Queue sd lien ket vong/src/Solution.java
+++ 11 DSA Stack Queue/Queue sd lien ket vong/src/Solution.java
@@ -2,20 +2,26 @@ public class Solution {
     Queue queue=new Queue();
     public void enQueue(int data) {
         Node newNode = new Node(data);
-        if (queue.rear==null) {
+        if (queue.front==null) {
             queue.front=queue.rear=newNode;
-            return;
+            queue.rear.link=queue.front;
+        }else {
+            queue.rear.link=newNode;
+            queue.rear=newNode;
+            queue.rear.link=queue.front;
         }
-        queue.rear.link=newNode;
-        queue.rear=newNode;
     }
     public void deQueue() {
         if (queue.front==null) {
             System.out.println("Hàng đợi trống");
             return;
         }
-        queue.front=queue.front.link;
-        if (queue.front==null) queue.rear=null;
+        if (queue.front==queue.rear) {
+            queue.front=queue.rear=null;
+        } else {
+            queue.front=queue.front.link;
+            queue.rear.link=queue.front;
+        }
     }
     public void displayQueue() {
         if (queue.front==null) {
@@ -24,10 +30,10 @@ public class Solution {
         }
         Node temp=queue.front;
         System.out.println("Queue: ");
-        while (temp != null) {
-            System.out.println(temp.data+ " ");
-            temp =temp.link;
-        }
+        do {
+            System.out.print(temp.data + " ");
+            temp = temp.link;
+        } while (temp != queue.front);
         System.out.println();
     }
 }
--- 11 DSA Stack Queue/Queue sd lien ket vong/src/Test.java
+++ 11 DSA Stack Queue/Queue sd lien ket vong/src/Test.java
@@ -1,11 +1,17 @@
 public class Test {
     public static void main(String[] args) {
         Solution s = new Solution();
-        s.enQueue(10);
-        s.enQueue(20);
-        s.enQueue(30);
+        s.enQueue(14);
+        s.enQueue(22);
+        s.enQueue(-6);
         s.displayQueue();
         s.deQueue();
+        s.displayQueue();
+        s.deQueue();
+        s.displayQueue();
+        s.enQueue(9);
+        s.displayQueue();
+        s.enQueue(20);
         s.displayQueue();
     }
 }
