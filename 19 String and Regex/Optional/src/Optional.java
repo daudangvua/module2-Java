873a4148154cf1ff592785c65102e48dd0f22d47
TAi <daudangvua@gmail.com>
2025-05-05 14:52:55 +0700

    add

--- 19 String and Regex/Optional/src/Optional.java
+++ 19 String and Regex/Optional/src/Optional.java
@@ -4,22 +4,24 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class CrawlSongExample {
+public class Optional {
     public static void main(String[] args) {
         try{
-            URL url = new URL("https://www.nhaccuatui.com/bai-hat/nhac-tre-moi.html");
+            URL url = new URL("http://dantri.com.vn/the-gioi.htm");
             Scanner scanner = new Scanner(new InputStreamReader(url.openStream()));
             scanner.useDelimiter("\\Z");
+
             String content = scanner.next();
             scanner.close();
+
             content = content.replaceAll("\\n+", "");
-            Pattern p = Pattern.compile("name_song\">(.*?)</a>");
+            Pattern p = Pattern.compile("class=\"title-news\">\\s*<a[^>]*title=\"(.*?)\"");
             Matcher m = p.matcher(content);
             while(m.find()){
                 System.out.println(m.group(1));
             }
         }catch(Exception e){
-            System.out.println(e.getMessage());
+            e.printStackTrace();
         }
     }
 }
